18da30a96875d867ff16193b5796b9e23ace240c
zhangjxm <zhangjxm@yonyou.com>
2020-01-05 23:27:55 +0800

    融合新增和编辑界面，整理controller，添加log和json

--- src/main/java/com/example/demo/controller/Operator_Controller.java
+++ src/main/java/com/example/demo/controller/Operator_Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.example.demo.entity.Operator;
 import com.example.demo.service.Operator_Service;
 import com.example.demo.utils.JsonInfo;
@@ -15,77 +16,94 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 @Controller
-@RequestMapping(value = {"Operator","zlb"})
+@RequestMapping(value = {"Operator", "zlb"})
 public class Operator_Controller {
-	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Operator_Controller.class);
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Operator_Controller.class);
 
-	@Autowired
-	Operator_Service oservice;
+    @Autowired
+    Operator_Service oservice;
 
+    //初始界面数据加载
+    @RequestMapping("index")
+    @ResponseBody
+    public HashMap<String, Object> index(String name, SearchInfo info) throws Exception {
+        log.info("[index]——name:" + name + "，info:" + JSON.toJSONString(info));
+        HashMap<String, Object> m = new HashMap<String, Object>();
+        // data
+        ArrayList<Operator> list = new ArrayList<Operator>();
+        String where = "";
+        if (name == null) {
+            where = "";
+        } else if (name == "") {
+            where = "";
+        } else {
+            where = "where nike like '%" + name + "%'";
+            info.setWhere(where);
+        }
+        list = (ArrayList<Operator>) oservice.index(info);
+        // count
+        int count = oservice.selectSize(info).get(0).getCount();
+        m.put("data", list);
+        m.put("count", count);
+        m.put("msg", "");
+        m.put("code", 0);
+        return m;
+    }
 
-	@RequestMapping("index")
-	@ResponseBody
-	public HashMap<String, Object> index(String name, SearchInfo info) throws Exception {
-		HashMap<String, Object> m = new HashMap<String, Object>();
-		// data
-		ArrayList<Operator> list = new ArrayList<Operator>();
-		String where = "";
-		if (name == null) {
-			where = "";
-		} else if (name == "") {
-			where = "";
-		} else {
-			where = "where nike like '%" + name + "%'";
-			info.setWhere(where);
-		}
-		list = (ArrayList<Operator>) oservice.index(info);
-		// count
-		int count = oservice.selectSize(info).get(0).getCount();
-		m.put("data", list);
-		m.put("count", count);
-		m.put("msg", "");
-		m.put("code", 0);
-		return m;
-	}
+    //进入新增界面
+    @RequestMapping("add")
+    @ResponseBody
+    public ModelAndView add() throws Exception {
+        log.info("[add]");
+        ModelAndView mv = new ModelAndView();
+//        mv.addObject("schoolName", oservice.getbyid(school_id));
+        mv.setViewName("operator/operatorEdit");
+        return mv;
+    }
 
-	// 添加管理员
-	@RequestMapping("insert")
-	protected @ResponseBody
-	JsonInfo insert(Operator o) {
-		int i = oservice.insert(o);
-		if (i > 0) {
-			return new JsonInfo(1, "添加成功！");
-		} else {
-			return new JsonInfo(0, "添加失败！");
-		}
-	}
-	
-	//修改管理员
-	@RequestMapping("update")
-	protected @ResponseBody
+    //进入编辑界面
+    @RequestMapping("edit")
+    public ModelAndView edit(int id) throws Exception {
+        log.info("[edit]——id：" + id);
+        ModelAndView mv = new ModelAndView();
+        Operator oper = oservice.getById(id);
+        mv.addObject("oper", oper);
+        mv.setViewName("operator/operatorEdit");
+        return mv;
+    }
+
+    //新增
+    @RequestMapping("insert")
+    protected @ResponseBody
+    JsonInfo insert(Operator o) {
+        log.info("[insert——operator:]" + JSON.toJSONString(o));
+        int i = oservice.insert(o);
+        if (i > 0) {
+            return new JsonInfo(1, "添加成功！");
+        } else {
+            return new JsonInfo(0, "添加失败！");
+        }
+    }
+
+    //编辑
+    @RequestMapping("update")
+    protected @ResponseBody
     JsonInfo update(Operator oper, ModelMap m) {
-		oservice.update(oper);
-		return new JsonInfo(1, "修改成功！");
-	}
-	
-	@RequestMapping("updatePass")
-	protected @ResponseBody
-    JsonInfo updatePass(int id, String password) {
-		oservice.updatePass(id);
-		return new JsonInfo(1, "密码重置成功!");
-	}
+        log.info("[update]——operator:" + JSON.toJSONString(oper));
+        oservice.update(oper);
+        return new JsonInfo(1, "修改成功！");
+    }
 
-	@RequestMapping("operatorEdit")
-	public ModelAndView testFreemarker(int id) throws Exception {
-		log.info("编辑操作员——id："+id);
-		ModelAndView mv = new ModelAndView();
-		Operator oper = oservice.getById(id);
-		mv.addObject("oper", oper);
-		mv.setViewName("operator/operatorUpdate");
-		return mv;
-	}
-	
-	//用户查看个人信息
+    //重置密码
+    @RequestMapping("resetPwd")
+    protected @ResponseBody
+    JsonInfo resetPwd(int id, String password) {
+        log.info("[resetPwd]——id：" + id + ",password：" + password);
+        oservice.updatePass(id);
+        return new JsonInfo(1, "密码重置成功!");
+    }
+
+    //用户查看个人信息
 //	@RequestMapping("personView")
 //	public ModelAndView personView(HttpSession sesssion) throws Exception {
 //		ModelAndView mv = new ModelAndView();
